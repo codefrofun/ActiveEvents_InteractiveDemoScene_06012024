4369198d586cb114726d36641d54cc28d21335ba
Finlayson <W0506675@campus.nscc.ca>
2025-01-06 15:14:34 -0400

    Initial commit

---
+++ Assets/Actionsmanager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actionsmanager : MonoBehaviour
+{
+    public static class ActionsMnagaer
+    { 
+
+        //custom action
+        public static Action RollyPolyOly;
+
+
+
+        public void OnEnable;
+        {
+
+        }
+
+        public void OnDisable;
+        {
+
+        }
+    }
+}
